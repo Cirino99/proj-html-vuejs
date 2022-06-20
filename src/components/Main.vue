343b6c4d1784206a96b7f70ba46c053646e5f542
Simone <cirino.simone99@gmail.com>
2022-06-20 12:03:14 +0200

    finita sezione specials nel main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,6 @@
 <template>
     <main>
+        <!-- phrases img -->
         <section class="d-flex" id="phrases-pizza">
             <div>
                 <img src="../assets/img/h3-img-1.jpg" alt="">
@@ -14,10 +15,58 @@
                 <img src="../assets/img/h3-img-4.jpg" alt="">
             </div>
         </section>
+        <!-- carosello -->
         <section id="carousel">
             <Carousel />
         </section>
-
+        <!-- special -->
+        <section class="d-flex" id="specials">
+            <div class="my-col d-flex justify-content-center align-items-center">
+                <img src="../assets/img/h1-img-7n.png" alt="">
+            </div>
+            <div class="my-col">
+                <div class="d-flex flex-column justify-content-center align-items-start" id="special-menu">
+                    <h2>SPECIALS</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, eveniet quae nam</p>
+                    <div class="d-flex">
+                        <div>
+                            <h3 class="prezzo">
+                                $10
+                            </h3>
+                        </div>
+                        <div class="prodotto">
+                            <h3>COMBO PICCOLO</h3>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor</p>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="prezzo">
+                            <h3>
+                                $10
+                            </h3>
+                        </div>
+                        <div class="prodotto">
+                            <h3>COMBO PICCOLO</h3>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor</p>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="prezzo">
+                            <h3>
+                                $10
+                            </h3>
+                        </div>
+                        <div class="prodotto">
+                            <h3>COMBO PICCOLO</h3>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor</p>
+                        </div>
+                    </div>
+                    <div class="icon-absolute d-flex justify-content-center align-items-center">
+                        <img src="../assets/svg/svg-4.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -49,6 +98,65 @@ main {
                 width: 100%;
                 height: auto;
             }
+        }
+    }
+
+    #specials {
+
+        .my-col {
+            width: 50%;
+            padding: 150px 0;
+
+            &:first-child {
+                background-image: url('../assets/img/h1-img-4.jpg');
+            }
+
+            #special-menu {
+                position: relative;
+                padding: 0 220px;
+
+                h2 {
+                    font-weight: 700;
+                    font-size: 38px;
+                }
+
+                p {
+                    color: #858585;
+                    margin-bottom: 20px;
+                }
+
+                div {
+                    .prezzo {
+                        color: #AB4027;
+                    }
+
+                    h3 {
+                        font-weight: 600;
+                        font-size: 22px;
+                    }
+
+                    .prodotto {
+                        padding-left: 20px;
+
+                        p {
+                            color: #8F8F8F;
+                        }
+                    }
+                }
+
+                .icon-absolute {
+                    position: absolute;
+                    bottom: 0;
+                    right: 20px;
+                    background-color: #f6f7f2;
+                    border-radius: 50%;
+                    width: 50px;
+                    height: 50px;
+                    padding: 0;
+                }
+            }
+
+
         }
     }
 }
