2471261159e97926b2d1f8badd9cd361e54c07f6
Simone <cirino.simone99@gmail.com>
2022-06-20 11:06:59 +0200

    prima section nel main fatta

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,20 @@
 <template>
-    <main></main>
+    <main>
+        <section class="d-flex" id="phrases-pizza">
+            <div>
+                <img src="../assets/img/h3-img-1.jpg" alt="">
+            </div>
+            <div>
+                <img src="../assets/img/h3-img-2.jpg" alt="">
+            </div>
+            <div>
+                <img src="../assets/img/h3-img-3.jpg" alt="">
+            </div>
+            <div>
+                <img src="../assets/img/h3-img-4.jpg" alt="">
+            </div>
+        </section>
+    </main>
 </template>
 
 <script>
@@ -9,4 +24,26 @@ export default {
 </script>
 
 <style scoped lang="scss">
+main {
+    #phrases-pizza {
+
+        div {
+            width: 25%;
+            padding: 10px 5px;
+
+            &:first-child {
+                padding-left: 0;
+            }
+
+            &:last-child {
+                padding-right: 0;
+            }
+
+            img {
+                width: 100%;
+                height: auto;
+            }
+        }
+    }
+}
 </style>
